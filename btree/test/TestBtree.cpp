70672bf45c34bf092d5d4402731f5ec2f39d2e6a
Yi Zhang <yizhang@cs.duke.edu>
2010-01-19 11:03:24 -0500

    test code updated

--- btree/test/TestBtree.cpp
+++ btree/test/TestBtree.cpp
@@ -3,20 +3,28 @@
 #include "../BtreeDLeafBlock.h"
 #include "../Btree.h"
 #include "../Splitter.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
 
 TEST(Btree, Create)
 {
-    // BSplitter sp(BtreeDLeafBlock::capacity);
-    MSplitter sp;
-    Btree tree("tree1.dat", 100, &sp, &sp);
-    tree.print();
+    ofstream out("m.txt");
+    
+    BSplitter bsp(BtreeDLeafBlock::capacity);
+    MSplitter msp;
+    Key_t dim = 10000;
+    Btree tree("tree1.dat", dim, &msp, &msp);
+    // tree.print();
     Key_t key = 0;
     Datum_t datum = key;
-    for (int i=1; i<28; i++) {
+    for (int i=0; i<dim; i++) {
         key = i;
-        datum = i;
+        datum = 1.0;
         tree.put(key, datum);
+        out<<i<<"\t"<<tree.getNumLeaves()<<"\n";
     }
-    tree.print();
+    cout<<"# leaves "<<tree.getNumLeaves()<<endl;
 
 }
