946c2ab63d63533f62a4700082f989d96da7a201
Yi Zhang <yizhang@cs.duke.edu>
2010-11-12 12:29:59 -0500

    add test for Btree-backed batch put

--- array/test/TestBatchPut.cpp
+++ array/test/TestBatchPut.cpp
@@ -15,14 +15,14 @@ void checkRect(const MDArray<2> &array, const MDCoord<2> &begin, const MDCoord<2
 		}
 }
 
-TEST(MDArray, BatchPut)
+TEST(MDArray, BatchPutDMA)
 {
-	MDCoord<2> dim(8,8);
+	MDCoord<2> dim(100,100);
 
-	const int rows = 4;
-	const int cols = 4;
+	const int rows = 20;
+	const int cols = 20;
 	const int size = rows * cols;
-	const MDCoord<2> begin(1,1);
+	const MDCoord<2> begin(3,5);
 	const MDCoord<2> end = begin + MDCoord<2>(rows,cols) - MDCoord<2>(1,1);
 	Datum_t data[size];
 	for (int i=0; i<size; ++i)
@@ -31,6 +31,38 @@ TEST(MDArray, BatchPut)
 	// put range [0,1]x[0,1]
 	RowMajor<2> rowMajor(&dim[0]);
 	MDArray<2> array(dim, DMA, &rowMajor, "array.bin");
+	array.batchPut(begin, end, data);
+	checkRect(array, begin, end, data);
+
+	ColMajor<2> colMajor(&dim[0]);
+	array.setLinearization(&colMajor);
+	array.batchPut(begin, end, data);
+	checkRect(array, begin, end, data);
+
+	i64 blockDims[] = {3,2};
+	u8 orders[] = {1, 0};
+	BlockBased<2> blockBased(&dim[0], blockDims, orders, orders);
+	array.setLinearization(&blockBased);
+	array.batchPut(begin, end, data);
+	checkRect(array, begin, end, data);
+}
+
+TEST(MDArray, BatchPutBtree)
+{
+	MDCoord<2> dim(100,100);
+
+	const int rows = 20;
+	const int cols = 20;
+	const int size = rows * cols;
+	const MDCoord<2> begin(3,5);
+	const MDCoord<2> end = begin + MDCoord<2>(rows,cols) - MDCoord<2>(1,1);
+	Datum_t data[size];
+	for (int i=0; i<size; ++i)
+		data[i] = i+1;
+
+	// put range [0,1]x[0,1]
+	RowMajor<2> rowMajor(&dim[0]);
+	MDArray<2> array(dim, BTREE, &rowMajor, "array.bin");
 	array.batchPut(begin, end, data);
 	checkRect(array, begin, end, data);
 
