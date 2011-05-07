418afa3608701016315674c780f88fb26116caba
Yi Zhang <yizhang@cs.duke.edu>
2011-05-07 16:36:11 -0400

    made linearization a cmd arg of loadmm

--- expr/mm/loadmm.cpp
+++ expr/mm/loadmm.cpp
@@ -1,5 +1,6 @@
 #include "common/Config.h"
 #include "array/ColMajor.h"
+#include "array/RowMajor.h"
 #include "array/MDArray.h"
 #include "btree/Btree.h"
 #include <iostream>
@@ -9,14 +10,16 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-    if (argc < 3) {
-        cerr<<"usage: "<<argv[0]<<" inputlist to_types(BLD)"<<endl
+    if (argc < 4) {
+        cerr<<"usage: "<<argv[0]
+            <<" inputlist linearization(CRB) to_types(BLD)"<<endl
             <<"inputlist: a file containing paths to MatrixMarket format files"<<endl
+            <<"linearization: Column, Row, or Block"<<endl
             <<"B: B-tree L: LAB-tree D: DirectlyMapped"<<endl;
         exit(1);
     }
     char types[256] = {0};
-    for (char *p=argv[2]; *p; ++p)
+    for (char *p=argv[3]; *p; ++p)
         types[int(*p)] = 1;
 
     char *inputfile = argv[1];
@@ -24,10 +27,28 @@ int main(int argc, char **argv)
     ifstream in(inputfile);
     // support two linearizations: col-major and block based
     i64 blockDims[] = {config->matmulBlockFactor, config->matmulBlockFactor};
-    //i64 blockDims[] = {500,500};
     u8 orders[] = {1,0};
-    BlockBased<2> l_bb(blockDims, blockDims, orders, orders);
-    ColMajor<2> l_col(blockDims); // bogus dim
+    Linearization<2> *lin = NULL;
+    const char *linstr;
+    // we use bogus array dims because the batch load ctor of MDArray
+    // will detect and apply the actual dim
+    switch (argv[2][0]) {
+    case 'B':
+        lin = new BlockBased<2>(blockDims, blockDims, orders, orders);
+        linstr = "bb";
+        break;
+    case 'C':
+        lin = new ColMajor<2> (blockDims);
+        linstr = "cm";
+        break;
+    case 'R':
+        lin = new RowMajor<2> (blockDims);
+        linstr = "rm";
+        break;
+    default:
+        cerr<<"unknown linearization"<<endl;
+    }
+
     int batchLoadBufferSize = blockDims[0] * blockDims[1];
     while (in.good()) {
         in.getline(path, 256);
@@ -41,60 +62,36 @@ int main(int argc, char **argv)
         sp.fileName = buf;
         int baselen = strlen(basename(path));
         strcpy(buf, basename(path));
-
-        // column major
+        // now the linearization part
+        buf[baselen++] = '.';
         buf[baselen] = '\0';
-        strcat(buf, ".cm.lab");
+        strcat(buf, linstr);
+        baselen += strlen(linstr);
+
+        buf[baselen] = '\0';
+        strcat(buf, ".lab");
         sp.type = BTREE;
         sp.leafSp = 'A';
         sp.intSp = 'M';
         sp.useDenseLeaf = true;
         if (types['L']) {
-            MDArray<2> array1(&sp, &l_col, "MM", path, batchLoadBufferSize);
+            MDArray<2> array1(&sp, lin, "MM", path, batchLoadBufferSize);
             //((Btree::BTree*)array1.storage)->print(true);
         }
 
         buf[baselen] = '\0';
-        strcat(buf, ".cm.b");
+        strcat(buf, ".b");
         sp.leafSp = 'M';
         sp.useDenseLeaf = false;
         if (types['B']) {
-            MDArray<2> array2(&sp, &l_col, "MM", path, batchLoadBufferSize);
+            MDArray<2> array2(&sp, lin, "MM", path, batchLoadBufferSize);
             //((Btree::BTree*)array2.storage)->print(true);
         }
+
         buf[baselen] = '\0';
-        strcat(buf, ".cm.dma");
+        strcat(buf, ".dma");
         sp.type = DMA;
         if (types['D'])
-            MDArray<2> array3(&sp, &l_col, "MM", path, batchLoadBufferSize);
-
-		/*
-        // block based
-        buf[baselen] = '\0';
-        strcat(buf, ".bb.lab");
-        sp.type = BTREE;
-        sp.leafSp = 'A';
-        sp.intSp = 'M';
-        sp.useDenseLeaf = true;
-        if (types['L']) {
-            MDArray<2> array4(&sp, &l_bb, "MM", path, batchLoadBufferSize);
-            //((Btree::BTree*)array4.storage)->print(true);
-        }
-
-        buf[baselen] = '\0';
-        strcat(buf, ".bb.b");
-        sp.leafSp = 'M';
-        sp.useDenseLeaf = false;
-        if (types['B']) {
-            MDArray<2> array5(&sp, &l_bb, "MM", path, batchLoadBufferSize);
-            //((Btree::BTree*)array5.storage)->print(true);
-        }
-
-        buf[baselen] = '\0';
-        strcat(buf, ".bb.dma");
-        sp.type = DMA;
-        if (types['D'])
-            MDArray<2> array6(&sp, &l_bb, "MM", path, batchLoadBufferSize);
-			*/
+            MDArray<2> array3(&sp, lin, "MM", path, batchLoadBufferSize);
     }
 }
